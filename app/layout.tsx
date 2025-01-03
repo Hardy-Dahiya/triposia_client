5d8ab39179d94044b9628989650c4b176376e86b
Ashok Bhamla <ashokbhamla@gmail.com>
2025-01-03 16:42:01 +0530

    Update layout.tsx
    css url Change

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,12 +26,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Link normal CSS files after preloading */}
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
-        <link
+        {/* Link normal CSS files after preloading 
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />*/}
+        <link rel="stylesheet" href="https://aerocloud.s3.amazonaws.com/style/triposia_style.css" />
+         {/*<link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        />
+        />*/}
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
