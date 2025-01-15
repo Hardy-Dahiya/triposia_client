c43f236b7bbf8b3c42d37f02278c1adefeff038a
hardy <dahiyahar@gmail.com>
2025-01-15 16:01:50 +0530

    added top hotels

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -3,6 +3,86 @@ import Header from '@/src/components/Header/Header';
 import Footer from '@/src/components/Footer/Footer';
 import Link from 'next/link';
 import Blogs from '@/src/components/Blogs/Blogs';
+import { Hotel } from '@/src/types/types';
+
+const topHotels = [
+  {
+    hotelId: 59877314,
+    hotelName: 'Dubai Hostel and Bedspace',
+    starRating: 5,
+    reviewScore: 5.6,
+    reviewCount: 2,
+    currency: 'USD',
+    dailyRate: 8.15,
+    crossedOutRate: 35.94,
+    discountPercentage: 30,
+    imageURL:
+      'http://pix8.agoda.net/hotelImages/59877314/-1/be1288084ca4c5d50f3321d3a4ea6b13.jpg?ce=0&s=800x600',
+    landingURL:
+      'https://www.agoda.com/partners/partnersearch.aspx?cid=1918595&hid=59877314&currency=USD&checkin=2024-09-02&checkout=2024-09-03&NumberofAdults=2&NumberofChildren=1&childages=17&Rooms=1&pcs=6',
+    includeBreakfast: false,
+    freeWifi: true,
+    latitude: 25.283611,
+    longitude: 55.399246,
+  },
+  {
+    hotelId: 68218,
+    hotelName: 'Abjad Crown Hotel',
+    starRating: 3,
+    reviewScore: 6.5,
+    reviewCount: 3249,
+    currency: 'USD',
+    dailyRate: 9.58,
+    crossedOutRate: 38.3,
+    discountPercentage: 0,
+    imageURL:
+      'http://pix8.agoda.net/hotelImages/68218/-1/0cd4a745c9f26f2492b6e3713f49518a.jpg?ce=0&s=800x600',
+    landingURL:
+      'https://www.agoda.com/partners/partnersearch.aspx?cid=1918595&hid=68218&currency=USD&checkin=2024-09-02&checkout=2024-09-03&NumberofAdults=2&NumberofChildren=1&childages=17&Rooms=1&pcs=6',
+    includeBreakfast: false,
+    freeWifi: true,
+    latitude: 25.2731186,
+    longitude: 55.3197938,
+  },
+  {
+    hotelId: 38369209,
+    hotelName: 'Royal Casa- Marina Diamond',
+    starRating: 0,
+    reviewScore: 5.3,
+    reviewCount: 13,
+    currency: 'USD',
+    dailyRate: 10.19,
+    crossedOutRate: 0,
+    discountPercentage: 0,
+    imageURL:
+      'http://q-xx.bstatic.com/xdata/images/hotel/840x460/442304127.jpg?k=c394b75affc3f99756a216ff3cfde3a925d6490dcee95900c8c9623376483d1c&o=',
+    landingURL:
+      'https://www.agoda.com/partners/partnersearch.aspx?cid=1918595&hid=38369209&currency=USD&checkin=2024-09-02&checkout=2024-09-03&NumberofAdults=2&NumberofChildren=1&childages=17&Rooms=1&pcs=6',
+    includeBreakfast: false,
+    freeWifi: true,
+    latitude: 25.069480895996094,
+    longitude: 55.1348991394043,
+  },
+  {
+    hotelId: 180212,
+    hotelName: 'Dorus Hotel',
+    starRating: 4,
+    reviewScore: 6,
+    reviewCount: 2741,
+    currency: 'USD',
+    dailyRate: 13.31,
+    crossedOutRate: 53.23,
+    discountPercentage: 0,
+    imageURL:
+      'http://pix8.agoda.net/hotelImages/180/180212/180212_15030314410025742626.jpg?ca=3&ce=1&s=800x600',
+    landingURL:
+      'https://www.agoda.com/partners/partnersearch.aspx?cid=1918595&hid=180212&currency=USD&checkin=2024-09-02&checkout=2024-09-03&NumberofAdults=2&NumberofChildren=1&childages=17&Rooms=1&pcs=6',
+    includeBreakfast: false,
+    freeWifi: true,
+    latitude: 25.2720937,
+    longitude: 55.31586589999999,
+  },
+];
 
 function Home() {
   return (
@@ -135,66 +215,26 @@ function Home() {
             <div className="column is-12">
               <h2 className="title is-4">Top Hotels</h2>
             </div>
-            <div className="column is-3">
-              <Link href="">
-                <img
-                  src="images/hotel1.jpg"
-                  alt=""
-                  className="image imgw100 img-radius-top-right"
-                />
-              </Link>
-              <div className="card-box">
-                <h3 className="title is-5 mb-4">
-                  <Link href="">Disneyland</Link>
-                </h3>
-                <p>We have over 28K reviews to assure you top notch service.</p>
-              </div>
-            </div>
-            <div className="column is-3">
-              <Link href="">
-                <img
-                  src="images/hotel2.jpg"
-                  alt=""
-                  className="image imgw100 img-radius-top-right"
-                />
-              </Link>
-              <div className="card-box">
-                <h3 className="title is-5 mb-4">
-                  <Link href="">Eiffel</Link>
-                </h3>
-                <p>We have over 28K reviews to assure you top notch service.</p>
-              </div>
-            </div>
-            <div className="column is-3">
-              <Link href="">
-                <img
-                  src="images/hotel3.jpg"
-                  alt=""
-                  className="image imgw100 img-radius-top-right"
-                />
-              </Link>
-              <div className="card-box">
-                <h3 className="title is-5 mb-4">
-                  <Link href="">Trimorph</Link>
-                </h3>
-                <p>We have over 28K reviews to assure you top notch service.</p>
-              </div>
-            </div>
-            <div className="column is-3">
-              <Link href="">
-                <img
-                  src="images/hotel4.jpg"
-                  alt=""
-                  className="image imgw100 img-radius-top-right"
-                />
-              </Link>
-              <div className="card-box">
-                <h3 className="title is-5 mb-4">
-                  <Link href="">Malena</Link>
-                </h3>
-                <p>We have over 28K reviews to assure you top notch service.</p>
-              </div>
-            </div>
+            {topHotels.map((item: Hotel, index: number) => {
+              return (
+                <div key={index} className="column is-3">
+                  <Link href={item.landingURL}>
+                    <img
+                      src={item.imageURL}
+                      alt={item.hotelName}
+                      className="image imgw100 img-radius-top-right"
+                      style={{ width: '318px', height: '212px' }}
+                    />
+                  </Link>
+                  <div className="card-box">
+                    <h3 className="title is-5 mb-4">
+                      <Link href={item.landingURL}>{item.hotelName}</Link>
+                    </h3>
+                    <p>We have over 28K reviews to assure you top notch service.</p>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </section>
