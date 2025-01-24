2e5ee6a7c4155316412ece683554b61823f243eb
hardy <dahiyahar@gmail.com>
2025-01-24 14:27:30 +0530

    a new hostname added for testing

--- services/pages/PageServices.tsx
+++ services/pages/PageServices.tsx
@@ -5,6 +5,7 @@ const domainMap: Record<string, number> = {
   'triposia.com': 1,
   'tripsearchs.com': 2,
   localhost: 2, // For development
+  'triposia-client.vercel.app': 2, // For development
   'flightsdetail.com': 3,
   'airport-terminals.com': 4,
   'airlinesmap.com': 5,
