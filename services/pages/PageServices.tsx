c8897d37d92f874293371fc60b07775913f97886
hardy <dahiyahar@gmail.com>
2025-01-24 14:23:39 +0530

    hostname api added

--- services/pages/PageServices.tsx
+++ services/pages/PageServices.tsx
@@ -1,13 +1,25 @@
 import axios from 'axios';
 import url from '../../src/api/api';
 
+const domainMap: Record<string, number> = {
+  'triposia.com': 1,
+  'tripsearchs.com': 2,
+  localhost: 2, // For development
+  'flightsdetail.com': 3,
+  'airport-terminals.com': 4,
+  'airlinesmap.com': 5,
+  'clearbeds.com': 6,
+};
+
 // getFlightPage
 const getFlightPage = async (
   flight_id: string | number | null,
   language_id: string | number | null,
 ) => {
   try {
-    const URL = `${url}/page/flight?flight_id=${flight_id}&language_id=${language_id}`;
+    const host = await axios.get('http://localhost:3000/api');
+    const domainId = domainMap[host.data.hostname];
+    const URL = `${url}/page/flight?flight_id=${flight_id}&language_id=${language_id}&domain_id=${domainId}`;
     return await axios.get(URL, {});
   } catch (error) {
     if (axios.isAxiosError(error)) {
@@ -29,7 +41,9 @@ const getAirportPage = async (
   language_id: string | number | null,
 ) => {
   try {
-    const URL = `${url}/page/airport?airport_id=${airport_id}&language_id=${language_id}`;
+    const host = await axios.get('http://localhost:3002/api');
+    const domainId = domainMap[host.data.hostname];
+    const URL = `${url}/page/airport?airport_id=${airport_id}&language_id=${language_id}&domain_id=${domainId}`;
     return await axios.get(URL, {});
   } catch (error) {
     if (axios.isAxiosError(error)) {
@@ -51,7 +65,9 @@ const getAirlinePage = async (
   language_id: string | number | null,
 ) => {
   try {
-    const URL = `${url}/page/airline?airline_id=${airline_id}&language_id=${language_id}`;
+    const host = await axios.get('http://localhost:3000/api');
+    const domainId = domainMap[host.data.hostname];
+    const URL = `${url}/page/airline?airline_id=${airline_id}&language_id=${language_id}&domain_id=${domainId}`;
     return await axios.get(URL, {});
   } catch (error) {
     if (axios.isAxiosError(error)) {
