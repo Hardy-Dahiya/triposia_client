8052bdec10059fc57655ac1e6c2f64609da61ef4
hardy <dahiyahar@gmail.com>
2025-01-27 12:15:09 +0530

    Meta Title and Keywords added for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,37 @@
 import Home from './pages/home/page';
+import { getPhone } from '@/services/phone/PhoneServices';
+import { Metadata } from 'next';
 
-export default function Page() {
+async function fetchPhone() {
+  try {
+    const response = await getPhone();
+    if (response?.data.status) {
+      return response.data.data;
+    }
+    return { phone: '' }; // Default value if data is missing
+  } catch (error) {
+    console.log(error);
+    return { phone: '' }; // Default value if data is missing
+  }
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  // Await `params` since it's treated as a Promise in the build environment
+  const phoneData = await fetchPhone();
+  if (phoneData) {
+    return {
+      title: phoneData.title,
+      description: phoneData.meta,
+      keywords: phoneData.keywords,
+    };
+  }
+  return {
+    title: 'TripOsia | Travel Made Easy',
+    description: `Plan your adventures effortlessly with Triposia! Discover top destinations, book accommodations, and explore curated travel experiences. Whether you're planning a weekend getaway or a dream vacation, Triposia helps you every step of the way. Your journey begins here!`,
+    keywords: 'TripOsia | Travel Made Easy',
+  };
+}
+
+export default async function Page() {
   return <Home />;
 }
