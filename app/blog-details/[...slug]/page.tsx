be2beb2b2267a97d1c912e9a8b399b1a130f9ff3
hardy <dahiyahar@gmail.com>
2025-02-13 17:19:10 +0530

    added auther bio

--- app/blog-details/[...slug]/page.tsx
+++ app/blog-details/[...slug]/page.tsx
@@ -105,11 +105,48 @@ async function Page({ slug }: { slug: string }) {
                       {getBlogData.created_at}{' '}
                     </span>
                   </div>
+                  <hr className="seprator my-5" />
                   <div className="single-blog-details-content-tags mt-3">
-                    <span className="single-blog-details-content-tags-item">
-                      {' '}
-                      <a href="javascript:void(0)">Author: {getBlogData.authors_name}</a>{' '}
-                    </span>
+                    <div className="columns is-multiline">
+                      <div className="au is-2 author">
+                        <img
+                          className="radius-5"
+                          src="https://blog.triposia.com/storage/photos/1/66431f78b21f2.jpg"
+                          alt="Stella Shon"
+                        />
+                      </div>
+                      <div className="aus is-10">
+                        <p>
+                          By{' '}
+                          <strong>
+                            <a href="/travel/author/stella_shon">Stella Shon</a>
+                          </strong>
+                        </p>
+                        <ul className="socials">
+                          {getBlogData.authors_linkedin ? (
+                            <li>
+                              <a href={getBlogData.authors_linkedin}>
+                                <i className="fa-brands fa-linkedin" />
+                              </a>
+                            </li>
+                          ) : (
+                            ''
+                          )}
+                          {getBlogData.authors_instagram ? (
+                            <li>
+                              <a href={getBlogData.authors_instagram}>
+                                <i className="fa-brands fa-instagram" />
+                              </a>
+                            </li>
+                          ) : (
+                            ''
+                          )}
+                        </ul>
+                        <ul className="socials">
+                          <li>Updated by - 2025-01-22T08:20:52.000000Z</li>
+                        </ul>
+                      </div>
+                    </div>
                   </div>
                   <div className="single-blog-details-thumb mt-4">
                     <img
@@ -132,246 +169,6 @@ async function Page({ slug }: { slug: string }) {
                   <p dangerouslySetInnerHTML={{ __html: getBlogData.content }} />
                 </div>
               </div>
-              <div className="details-tag-area color-two pat-25 pab-50">
-                <div className="columns is-multiline">
-                  <div className="column is-6 mt-4">
-                    <div className="blog-details-share-content">
-                      <h4 className="blog-details-share-content-title"> Share: </h4>
-                      <ul className="blog-details-share-social list-style-none">
-                        <li className="blog-details-share-social-list">
-                          <a
-                            className="blog-details-share-social-list-icon"
-                            href="javascript:void(0)"
-                          >
-                            {' '}
-                            <i className="fa-brands fa-facebook-f" />{' '}
-                          </a>
-                        </li>
-                        <li className="blog-details-share-social-list">
-                          <a
-                            className="blog-details-share-social-list-icon"
-                            href="javascript:void(0)"
-                          >
-                            {' '}
-                            <i className="fa-brands fa-twitter" />{' '}
-                          </a>
-                        </li>
-                        <li className="blog-details-share-social-list">
-                          <a
-                            className="blog-details-share-social-list-icon"
-                            href="javascript:void(0)"
-                          >
-                            {' '}
-                            <i className="fa-brands fa-instagram" />{' '}
-                          </a>
-                        </li>
-                        <li className="blog-details-share-social-list">
-                          <a
-                            className="blog-details-share-social-list-icon"
-                            href="javascript:void(0)"
-                          >
-                            {' '}
-                            <i className="fa-brands fa-youtube" />{' '}
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="column is-6 mt-4">
-                    <div className="blog-details-share-content right-align">
-                      <h4 className="blog-details-share-content-title"> Tags: </h4>
-                      <ul className="blog-details-tag list-style-none">
-                        <li className="blog-details-tag-list">
-                          <a className="blog-details-tag-list-item" href="javascript:void(0)">
-                            {' '}
-                            New{' '}
-                          </a>
-                        </li>
-                        <li className="blog-details-tag-list">
-                          <a className="blog-details-tag-list-item" href="javascript:void(0)">
-                            {' '}
-                            Luxurious{' '}
-                          </a>
-                        </li>
-                        <li className="blog-details-tag-list">
-                          <a className="blog-details-tag-list-item" href="javascript:void(0)">
-                            {' '}
-                            Simple{' '}
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="comment-area">
-                <h3 className="details-section-title"> Comments(03) </h3>
-                <div className="row">
-                  <div className="col-lg-12 mt-2">
-                    <div className="comment-show-contents">
-                      <ul
-                        className="comment-list list-style-none wow fadeInLeft"
-                        data-wow-delay=".1s"
-                        style={{
-                          visibility: 'visible',
-                          animationDelay: '0.1s',
-                          animationName: 'fadeInLeft',
-                        }}
-                      >
-                        <li>
-                          <div className="blog-details-flex-content">
-                            <div className="blog-details-thumb radius-10">
-                              <img src="../../images/details-comment1.jpg" alt="" />
-                            </div>
-                            <div className="blog-details-content">
-                              <div className="blog-details-content-flex">
-                                <div className="blog-details-content-item">
-                                  <h5 className="blog-details-content-title">
-                                    {' '}
-                                    <a href="javascript:void(0)"> Riyad Hossain </a>{' '}
-                                  </h5>
-                                  <span className="blog-details-content-date"> Jun 22, 2022 </span>
-                                </div>
-                                <a href="javascript:void(0)" className="btn-replay">
-                                  {' '}
-                                  <i className="fa-solid fa-reply" /> Reply{' '}
-                                </a>
-                              </div>
-                              <p className="blog-details-content-para">
-                                {' '}
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a
-                                egestas leo. Aliquam ut ante lobortis tellus cursus pellentesque.
-                                Praesent feugiat tellus quis aliquet{' '}
-                              </p>
-                            </div>
-                          </div>
-                          <ul
-                            className="comment-list list-style-none wow fadeInLeft"
-                            data-wow-delay=".2s"
-                            style={{
-                              visibility: 'visible',
-                              animationDelay: '0.2s',
-                              animationName: 'fadeInLeft',
-                            }}
-                          >
-                            <li>
-                              <div className="blog-details-flex-content">
-                                <div className="blog-details-thumb radius-10">
-                                  <img src="../../images/details-comment2.jpg" alt="" />
-                                </div>
-                                <div className="blog-details-content">
-                                  <div className="blog-details-content-flex">
-                                    <div className="blog-details-content-item">
-                                      <h5 className="blog-details-content-title">
-                                        {' '}
-                                        <a href="javascript:void(0)"> Rajia Akter </a>{' '}
-                                      </h5>
-                                      <span className="blog-details-content-date">
-                                        {' '}
-                                        Jun 23, 2022{' '}
-                                      </span>
-                                    </div>
-                                    <a href="javascript:void(0)" className="btn-replay">
-                                      {' '}
-                                      <i className="fa-solid fa-reply" /> Reply{' '}
-                                    </a>
-                                  </div>
-                                  <p className="blog-details-content-para">
-                                    {' '}
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a
-                                    egestas leo. Aliquam ut ante lobortis tellus cursus
-                                    pellentesque. Praesent feugiat tellus quis aliquet{' '}
-                                  </p>
-                                </div>
-                              </div>
-                            </li>
-                          </ul>
-                        </li>
-                      </ul>
-                      <ul
-                        className="comment-list list-style-none wow fadeInLeft"
-                        data-wow-delay=".3s"
-                        style={{
-                          visibility: 'visible',
-                          animationDelay: '0.3s',
-                          animationName: 'fadeInLeft',
-                        }}
-                      >
-                        <li>
-                          <div className="blog-details-flex-content">
-                            <div className="blog-details-thumb radius-10">
-                              <img src="../../images/details-comment1.jpg" alt="" />
-                            </div>
-                            <div className="blog-details-content">
-                              <div className="blog-details-content-flex">
-                                <div className="blog-details-content-item">
-                                  <h5 className="blog-details-content-title">
-                                    {' '}
-                                    <a href="javascript:void(0)"> Bryn Colon </a>{' '}
-                                  </h5>
-                                  <span className="blog-details-content-date"> Jun 24, 2022 </span>
-                                </div>
-                                <a href="javascript:void(0)" className="btn-replay">
-                                  {' '}
-                                  <i className="fa-solid fa-reply" /> Reply{' '}
-                                </a>
-                              </div>
-                              <p className="blog-details-content-para">
-                                {' '}
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a
-                                egestas leo. Aliquam ut ante lobortis tellus cursus pellentesque.
-                                Praesent feugiat tellus quis aliquet{' '}
-                              </p>
-                            </div>
-                          </div>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="comment-area">
-                <h3 className="details-section-title"> Post Your Comment </h3>
-                <div className="row">
-                  <div className="col-lg-12 pat-20">
-                    <div className="details-comment-content">
-                      <div className="comments-flex-item">
-                        <div className="single-commetns">
-                          <label className="comment-label"> Your Name </label>
-                          <input
-                            type="text"
-                            className="form--control radius-5"
-                            name="name"
-                            placeholder="Type Name"
-                          />
-                        </div>
-                        <div className="single-commetns">
-                          <label className="comment-label"> Email Address </label>
-                          <input
-                            type="text"
-                            className="form--control radius-5"
-                            name="email"
-                            placeholder="Type Email"
-                          />
-                        </div>
-                      </div>
-                      <div className="single-commetns">
-                        <label className="comment-label"> Comment </label>
-                        <textarea
-                          name="message"
-                          className="form--control radius-5 form--message"
-                          placeholder="Post Comments"
-                          defaultValue={''}
-                        />
-                      </div>
-                      <button type="submit" className="submit-btn radius-5 mt-4">
-                        {' '}
-                        Post Comment{' '}
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
           <div className="column is-4">
@@ -380,74 +177,7 @@ async function Page({ slug }: { slug: string }) {
                 <div className="blog-details-side-title">
                   <h5 className="title"> Recent Post </h5>
                   <div className="blog-details-side-inner">
-                    <ul className="recent-list list-style-none">
-                      <li className="recent-list-item">
-                        <div className="recent-list-thumb">
-                          <a href="#0">
-                            {' '}
-                            <img
-                              className="lazyloads radius-5"
-                              src="../../images/recent-post1.jpg"
-                              alt=""
-                            />{' '}
-                          </a>
-                        </div>
-                        <div className="recent-list-contents">
-                          <h6 className="recent-list-title fs-16">
-                            {' '}
-                            <a href="#0"> Choose From a Wide Range of Properties </a>{' '}
-                          </h6>
-                          <span className="recent-list-dates light-color fs-14 mt-2">
-                            {' '}
-                            21 Jun 2022{' '}
-                          </span>
-                        </div>
-                      </li>
-                      <li className="recent-list-item">
-                        <div className="recent-list-thumb">
-                          <a href="#0">
-                            {' '}
-                            <img
-                              className="lazyloads radius-5"
-                              src="../../images/recent-post2.jpg"
-                              alt=""
-                            />{' '}
-                          </a>
-                        </div>
-                        <div className="recent-list-contents">
-                          <h6 className="recent-list-title fs-16">
-                            {' '}
-                            <a href="#0"> Choose From a Wide Range of Properties </a>{' '}
-                          </h6>
-                          <span className="recent-list-dates light-color fs-14 mt-2">
-                            {' '}
-                            22 Jun 2022{' '}
-                          </span>
-                        </div>
-                      </li>
-                      <li className="recent-list-item">
-                        <div className="recent-list-thumb">
-                          <a href="#0">
-                            {' '}
-                            <img
-                              className="lazyloads radius-5"
-                              src="../../images/recent-post3.jpg"
-                              alt=""
-                            />{' '}
-                          </a>
-                        </div>
-                        <div className="recent-list-contents">
-                          <h6 className="recent-list-title fs-16">
-                            {' '}
-                            <a href="#0"> Choose From a Wide Range of Properties </a>{' '}
-                          </h6>
-                          <span className="recent-list-dates light-color fs-14 mt-2">
-                            {' '}
-                            24 Jun 2022{' '}
-                          </span>
-                        </div>
-                      </li>
-                    </ul>
+                    <ul className="recent-list list-style-none"></ul>
                   </div>
                 </div>
               </div>
