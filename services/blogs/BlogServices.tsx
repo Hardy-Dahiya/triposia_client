bfb8a993328f3b750a9a9e2c75cc9b760b7bf5e1
hardy <dahiyahar@gmail.com>
2025-02-14 11:51:59 +0530

    added domainMap to blog pages

--- services/blogs/BlogServices.tsx
+++ services/blogs/BlogServices.tsx
@@ -1,5 +1,17 @@
 import axios from 'axios';
 // get Blogs
+const domainMap: Record<string, number> = {
+  'triposia.com': 1,
+  'tripsearchs.com': 2,
+  'localhost:3000': 2, // For development
+  'localhost:3002': 2, // For development
+  'triposia-client.vercel.app': 1, // For development
+  'flightsdetail.com': 3,
+  'airport-terminals.com': 4,
+  'airlinesmap.com': 5,
+  'clearbeds.com': 6,
+  'faresinfo.com': 7,
+};
 const getBlogs = async () => {
   try {
     const URL = `https://blog.triposia.com/blog/1/1`;
@@ -18,8 +30,9 @@ const getBlogs = async () => {
   }
 };
 // get Blogs details
-const getBlogsDetail = async (slug: string | null, domainID: string) => {
+const getBlogsDetail = async (slug: string | null, host: string) => {
   try {
+    const domainID = domainMap[host];
     const URL = `https://blog.triposia.com/blogdetails/${domainID}/1/${slug}`;
     return await axios.get(URL, {});
   } catch (error) {
